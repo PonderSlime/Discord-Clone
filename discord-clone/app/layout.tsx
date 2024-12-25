82010ad03f3a75c7c922d8bae1d460c8a3244f6c
PonderSlime <micah.j.edwards1@gmail.com>
2024-12-24 20:31:53 -0700

    Completely finished main sidebar!

--- discord-clone/app/layout.tsx
+++ discord-clone/app/layout.tsx
@@ -1,9 +1,11 @@
 import type { Metadata } from "next";
 import { Open_Sans} from "next/font/google";
 import { ClerkProvider } from '@clerk/nextjs'
-import "./globals.css";
-import { ThemeProvider } from "@/components/providers/theme-provider";
 import { cn } from "@/lib/utils";
+import "./globals.css";
+
+import { ThemeProvider } from "@/components/providers/theme-provider";
+import { ModalProvider } from "@/components/providers/modal-provider";
 
 const font = Open_Sans({
   variable: "--font-open-sans",
@@ -33,6 +35,7 @@ export default function RootLayout({
           enableSystem={false}
           storageKey="discord-theme"
         >
+          <ModalProvider />
           {children}
         </ThemeProvider>
       </body>
